fa2c61094921bcd5cb176692a54e934946ea2284
IDRALOU <54266864+IDRALOU@users.noreply.github.com>
2021-12-28 15:55:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import bypass_functions
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-#linkvertiseSites = ["linkvertise.com", "link-to.net", "up-to-down.net", "direct-link.net", "file-link.net"]
+linkvertiseSites = ["linkvertise.com", "link-to.net", "up-to-down.net", "direct-link.net", "file-link.net"]
 
 @app.route("/")
 def home():
@@ -18,10 +18,9 @@ def bypass():
     if not "https://" in url:
         if not "http://" in url:
             return "URL invalide."
-    #for linkvertiseSite in linkvertiseSites:
-        #if linkvertiseSite in url:
-            #return bypass_functions.bypassLinkvertise(url)
-            #break
+    for linkvertiseSite in linkvertiseSites:
+        if linkvertiseSite in url:
+            return bypass_functions.bypassLinkvertise(url)
     if "rekonise.com" in url:
         return bypass_functions.bypassRekonise(url)
     elif "sub2unlock.com" in url:
@@ -44,15 +43,6 @@ def bypass():
         return bypass_functions.bypassShortUrlLink(url)
     else:
         return "URL invalide."
-    
-@app.route("/api/unshort", methods=["GET"])
-def unshort():
-    url = request.args.get("url")
-    if url == None:
-        return "Une URL doit être spécifiée."
-    if not "https://" in url:
-        return "URL invalide."
-    return bypass_functions.unshortenerLink(url)
     
 
 if __name__ == "__main__":
